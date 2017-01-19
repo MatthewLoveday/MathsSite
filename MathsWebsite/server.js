2c94723b68c149ffc890404fffcc09dec331a0fa
Jonathan <jonathanwoollettlight@gmail.com>
2017-01-19 23:45:09 +0000

    prep for parsing data to marking

--- MathsWebsite/server.js
+++ MathsWebsite/server.js
@@ -279,7 +279,10 @@ app.post("/users/:id/tests",function(req,res)//when seed program ready this shou
     //     }
     // });
 });
+app.get("/users/:id/tests/edit", isLoggedIn, function (req, res) {
 
+    aler("hit route");
+});
 //---------------------------------------------------------------------
 //admin routes
 //---------------------------------------------------------------------
@@ -449,7 +452,7 @@ function isLoggedIn(req,res,next)
 
 function GenerateTest(time, topics) //Random Seed, Time of the Test, Array of the Topics
 { 
-    var questions=[]; //Questions array to be returned.
+    var returnTopics=[]; //Questions array to be returned.
 
     var topicTime = Math.floor(time / topics.length); //The time allocated to each topic the user has selected. 
     console.log("topicTime:" + topicTime);
@@ -457,12 +460,12 @@ function GenerateTest(time, topics) //Random Seed, Time of the Test, Array of th
 
     for(var i = 0; i < topics.length; i++)
     {
-        questions.push({ name:topics[i].name, questions:GetTopicQuestion(topics[i], topicTime) });
+        returnTopics.push({ name:topics[i].name, questions:GetTopicQuestion(topics[i], topicTime) });
     }
 
-    console.log("\n\nquestions (outside function)\n" + questions);
+    console.log("\n\nquestions (outside function)\n" + returnTopics);
 
-    return questions;
+    return returnTopics;
 }
 
 //So this is used to get a question within a topic that I pass in.
@@ -593,6 +596,8 @@ function getRandomIntInclusive(min, max) {
 //JWL functions
 //---------------------------------------------------------------------
 
+
+
 function searchArray(array, value)
 {
     for (var i = 0; i <= array.length; i++)
@@ -615,65 +620,3 @@ app.listen(process.env.PORT, process.env.IP,function()
 });
 
 
-/*
-Somebody once told me the world is gonna roll me
-I ain't the sharpest tool in the shed
-She was looking kind of dumb with her finger and her thumb
-In the shape of an "L" on her forehead
-
-Well the years start coming and they don't stop coming
-Fed to the rules and I hit the ground running
-Didn't make sense not to live for fun
-Your brain gets smart but your head gets dumb
-So much to do, so much to see
-So what's wrong with taking the back streets?
-You'll never know if you don't go
-You'll never shine if you don't glow
-
-Hey now, you're an all-star, get your game on, go play
-Hey now, you're a rock star, get the show on, get paid
-And all that glitters is gold
-Only shooting stars break the mold
-
-It's a cool place and they say it gets colder
-You're bundled up now, wait till you get older
-But the meteor men beg to differ
-Judging by the hole in the satellite picture
-The ice we skate is getting pretty thin
-The water's getting warm so you might as well swim
-My world's on fire, how about yours?
-That's the way I like it and I never get bored
-
-Hey now, you're an all-star, get your game on, go play
-Hey now, you're a rock star, get the show on, get paid
-All that glitters is gold
-Only shooting stars break the mold
-
-Hey now, you're an all-star, get your game on, go play
-Hey now, you're a rock star, get the show, on get paid
-And all that glitters is gold
-Only shooting stars
-
-Somebody once asked could I spare some change for gas?
-I need to get myself away from this place
-I said yep what a concept
-I could use a little fuel myself
-And we could all use a little change
-
-Well, the years start coming and they don't stop coming
-Fed to the rules and I hit the ground running
-Didn't make sense not to live for fun
-Your brain gets smart but your head gets dumb
-So much to do, so much to see
-So what's wrong with taking the back streets?
-You'll never know if you don't go (go!)
-You'll never shine if you don't glow
-
-Hey now, you're an all-star, get your game on, go play
-Hey now, you're a rock star, get the show on, get paid
-And all that glitters is gold
-Only shooting stars break the mold
-
-And all that glitters is gold
-Only shooting stars break the mold
-*/
